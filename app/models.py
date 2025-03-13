3bfcd6d0f33ca49a9e89bdcbd48d4fe793680a4c
Thalente Halaliso Sikhakhane <sikhakhanethalente162@gmail.com>
2025-03-13 10:09:46 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask_login import UserMixin
 from app import db
 
-class User(db.Model, UserMixin)
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
